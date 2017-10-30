c8b01c0c7aa24723abdd6f2c8963ec8b0289ff9d
Samuel Path <samuel.path@gmail.com>
2017-10-30 10:50:35 +0100

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+// Do work here
+router.get("/", (req, res) => {
+  const sam = { name: "sam", age: 100, cool: true };
+  // res.send("Hey! It works!");
+  // res.json(sam);
+  res.json(req.query);
+});
+
+router.get("/reverse/:name", (req, res) => {
+  const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse);
+});
+
+module.exports = router;
