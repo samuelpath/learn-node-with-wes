77305a23c4aa166d8800c8a722c498fd9dc528c7
Samuel Path <samuel.path@gmail.com>
2017-11-06 17:51:12 +0100

    up to lesson 14 05:15

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,8 +1,26 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
-  console.log(req.name);
-  res.render("index");
+  res.render('index');
 };
 
 exports.addStore = (req, res) => {
-  res.render("editStore", { title: "Add Store" });
+  res.render('editStore', { title: 'Add Store' });
 };
+
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  req.flash(
+    'success',
+    `Successfully created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {};
