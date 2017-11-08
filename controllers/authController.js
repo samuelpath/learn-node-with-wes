c819740b95d7517093e3f7aec4874950d144a0b4
Samuel Path <samuel.path@gmail.com>
2017-11-08 12:20:54 +0100

    up to less27 08:07

---
+++ controllers/authController.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const crypto = require('crypto');
+
+exports.login = passport.authenticate('local', {
+  failureRedirect: '/login',
+  failureFlash: 'Failed Login!',
+  successRedirect: '/',
+  successFlash: 'You are now logged in!'
+});
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out!');
+  res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+    return;
+  }
+  req.flash('error', 'Oops! You must be logged in in order to do that!');
+  res.redirect('/login');
+};
+
+exports.forgot = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    req.flash('error', 'No account with that email exists');
+    return res.redirect('/login');
+  }
+  user.resetPasswordToken = crypto.randomBytes(20).toString(hex);
+  user.resetPasswordExpires = Date.now() + 360000;
+};
